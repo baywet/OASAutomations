c12c3e78fc01200786a0851182d53b0ac29bd1fc
Vincent Biret <vincentbiret@hotmail.com>
2023-11-22 10:02:42 -0500

    - temporarily switches to minutes
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- scripts/label-no-recent.ps1
+++ scripts/label-no-recent.ps1
@@ -1,4 +1,4 @@
-$inactivityDelay = [timespan]::FromDays([int]::Parse($Env:NO_RECENT_ACTIVITY_DURATION_IN_DAYS))
+$inactivityDelay = [timespan]::FromMinutes([int]::Parse($Env:NO_RECENT_ACTIVITY_DURATION_IN_DAYS))
 $oldIssues = gh issue list --label "$Env:NEEDS_AUTHOR_FEEDBACK_LABEL" --state open --limit 100 --json number,author,createdAt | ConvertFrom-Json | Where-Object {$_.labels.name -notcontains $Env:NO_RECENT_ACTIVITY_LABEL }
 foreach($oldIssue in $oldIssues) {
 	$lastComment = gh issue view $oldIssue.number --json comments | ConvertFrom-Json | Select-Object -ExpandProperty comments | Where-Object {$_.author.login -eq $oldIssue.author.login} | Select-Object -Last 1
