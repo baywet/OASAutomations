29b99a1fe67f3325b5ce9f5e40bb7ddace9a7ee8
Vincent Biret <vincentbiret@hotmail.com>
2023-11-22 10:19:11 -0500

    - fixes date time parsing
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- scripts/close-no-recent.ps1
+++ scripts/close-no-recent.ps1
@@ -3,12 +3,12 @@ $oldIssues = gh issue list --label "$Env:NO_RECENT_ACTIVITY_LABEL" --state open 
 foreach($oldIssue in $oldIssues) {
 	$lastComment = gh issue view $oldIssue.number --json comments | ConvertFrom-Json | Select-Object -ExpandProperty comments | Where-Object {$_.author.login -eq $oldIssue.author.login} | Select-Object -Last 1
 	if($null -eq $lastComment) {
-		$lastCommentDate = $null
+		$lastCommentDate = [datetime]$null
 	} else {
-		$lastCommentDate = [datetime]::Parse($lastComment.createdAt)
+		$lastCommentDate = $lastComment.createdAt #powershell already parses the date for us with the json conversion
 	}
 	$lastLabelEvent = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$($Env:ORG_NAME)/$($Env:REPO_NAME)/issues/$($oldIssue.number)/events?per_page=100" | ConvertFrom-Json | Where-Object {$_.event -eq "labeled" -and $_.label.name -eq "$Env:NO_RECENT_ACTIVITY_LABEL"} | Select-Object -Last 1
-	$lastLabelEventDate = [datetime]::Parse($lastLabelEvent.created_at)
+	$lastLabelEventDate = $lastLabelEvent.created_at
 	if ($null -ne $lastCommentDate -and $lastCommentDate -gt $lastLabelEventDate) {
 		gh issue edit $oldIssue.number --remove-label "$Env:NO_RECENT_ACTIVITY_LABEL" --remove-label "$Env:NEEDS_AUTHOR_FEEDBACK_LABEL" --add-label "$Env:NEEDS_ATTENTION_LABEL"
 	} elseif (([datetime]::UtcNow - $lastCommentDate) -ge $inactivityDelay) {
